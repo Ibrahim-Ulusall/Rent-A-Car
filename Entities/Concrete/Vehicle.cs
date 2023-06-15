090eeee606f030781be852fcd88ee65ebc84d585
ibrahim <ibrahimulusal185@gmail.com>
2023-06-15 23:44:20 +0300

    EntityFramework Tabloları oluşturuluyor

---
+++ Entities/Concrete/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+	public class Vehicle:IEntity
+	{
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public int ModelId { get; set; }
+        public int ColorId { get; set; }
+        public int RateOfEngineId { get; set; }
+        public int CarDetailsId { get; set; }
+    }
+}
