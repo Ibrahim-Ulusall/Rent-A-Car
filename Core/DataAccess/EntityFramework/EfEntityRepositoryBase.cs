08215563546959e6d62d6cca9c1b74407da790c6
ibrahim <ibrahimulusal185@gmail.com>
2023-06-16 00:40:34 +0300

    Core Katmanı Güncellendi ve DataAccess Katmanı Abstract Sınıfları oluşturuldu

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -15,27 +15,50 @@ namespace Core.DataAccess.EntityFramework
 	{
 		public void Delete(TEntity entity)
 		{
-			throw new NotImplementedException();
+			using (TContext context = new TContext())
+			{
+				var deleteEntity = context.Entry(entity);
+				deleteEntity.State = EntityState.Deleted;
+				context.SaveChanges();
+			}
 		}
 
 		public TEntity Get(Expression<Func<TEntity, bool>> filter)
 		{
-			throw new NotImplementedException();
+			using (TContext context = new TContext())
+			{
+				return context.Set<TEntity>().First(filter);
+			}
 		}
 
 		public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
 		{
-			throw new NotImplementedException();
+			using (TContext context = new TContext())
+			{
+				return filter == null
+					? context.Set<TEntity>().ToList()
+					: context.Set<TEntity>().Where(filter).ToList();
+			}
 		}
 
 		public void Insert(TEntity entity)
 		{
-			throw new NotImplementedException();
+			using (TContext context = new TContext())
+			{
+				var addedEntity = context.Entry(entity);
+				addedEntity.State = EntityState.Added;
+				context.SaveChanges();
+			}
 		}
 
 		public void Update(TEntity entity)
 		{
-			throw new NotImplementedException();
+			using (TContext context = new TContext())
+			{
+				var updateEntity = context.Entry(entity);
+				updateEntity.State = EntityState.Modified;
+				context.SaveChanges();
+			}
 		}
 	}
 }
