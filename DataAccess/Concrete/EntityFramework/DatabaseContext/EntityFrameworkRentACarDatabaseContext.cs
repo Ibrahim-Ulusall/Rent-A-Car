a050a4c6a8959dc409b659836b9d29599be47d91
ibrahim <ibrahimulusal185@gmail.com>
2023-06-16 01:11:03 +0300

    Veritabanı konfigrasyonu yapıldı!

--- DataAccess/Concrete/EntityFramework/DatabaseContext/EntityFrameworkRentACarDatabaseContext.cs
+++ DataAccess/Concrete/EntityFramework/DatabaseContext/EntityFrameworkRentACarDatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,22 @@ namespace DataAccess.Concrete.EntityFramework.DatabaseContext
 {
     public class EntityFrameworkRentACarDatabaseContext : DbContext
     {
-    }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=CarProjectDatabase;Trusted_Connection=true;");
+		}
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+		public DbSet<Model> Models { get; set; }
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Color> Colors { get; set; }
+		public DbSet<RateOfEngine> RateOfEngines { get; set; }
+		public DbSet<CarDetail> CarDetails { get; set; }
+		public DbSet<Rental> Rentals { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<City> Cities { get; set; }
+		public DbSet<District> Districts { get; set; }
+
+
+	}
 }
