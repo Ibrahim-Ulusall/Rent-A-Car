e2e0f172607ba15359ec2b9c5b12ab4019449114
ibrahim <ibrahimulusal185@gmail.com>
2023-06-16 00:19:54 +0300

    Core Katmanı DataAccess Layer Oluşturuldu ve Temel Metotlar Eklendi

---
+++ Core/DataAccess/EntityFramework/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+	public interface IEntityRepository<TEntity> where TEntity : class,IEntity,new()
+	{
+		void Insert(TEntity entity);
+		void Delete(TEntity entity);
+		void Update(TEntity entity);
+		List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+		TEntity Get(Expression<Func<TEntity, bool>> filter);
+	}
+}
