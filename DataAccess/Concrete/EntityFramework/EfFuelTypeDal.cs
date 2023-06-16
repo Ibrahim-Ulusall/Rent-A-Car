c7f64b7c452acba0fb1cebd8fd8459b38dac359e
ibrahim <ibrahimulusal185@gmail.com>
2023-06-16 13:00:16 +0300

    DataAccess Alanları oluşturuldu

---
+++ DataAccess/Concrete/EntityFramework/EfFuelTypeDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.DatabaseContext;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class EfFuelTypeDal:EfEntityRepositoryBase<FuelType,
+		EntityFrameworkRentACarDatabaseContext>,IFuelTypeDal
+	{
+	}
+}
