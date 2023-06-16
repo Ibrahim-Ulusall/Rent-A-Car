0d419cd13555224da359cd36c51ec666ff90ef38
ibrahim <ibrahimulusal185@gmail.com>
2023-06-16 13:16:48 +0300

    BusinessKatmanı Güncellendi!

---
+++ Business/Concrete/StatusServiceManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+	public class StatusServiceManager : IStatusService
+	{
+		IStatusDal _statusDal;
+        public StatusServiceManager(IStatusDal statusDal)
+        {
+            _statusDal = statusDal;
+        }
+        public List<Status> GetAll()
+		{
+			return _statusDal.GetAll();
+		}
+	}
+}
