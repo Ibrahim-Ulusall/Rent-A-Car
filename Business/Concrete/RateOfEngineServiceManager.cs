478b904a2bd83d65e585705ebee632e81a423823
ibrahim <ibrahimulusal185@gmail.com>
2023-06-28 19:08:59 +0300

    RateOfEngineServiceManager Eklendi!

---
+++ Business/Concrete/RateOfEngineServiceManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Business.Contans;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+	public class RateOfEngineServiceManager:ManagerBase<RateOfEngine,IRateOfEngineDal> , IRateOfEngineService
+	{
+        public RateOfEngineServiceManager(IRateOfEngineDal rateOfEngineDal) : base(rateOfEngineDal) { }
+
+		public override IDataResult<RateOfEngine> Get(RateOfEngine entity)
+		{
+			try
+			{
+				RateOfEngine existingEntity = _entityDal.Get(engine => engine.EngineId == entity.EngineId);
+				if (entity == null)
+					return new ErrorDataResult<RateOfEngine>(Messages.NullEntityError);
+				else if (existingEntity == null)
+					return new ErrorDataResult<RateOfEngine>(Messages.ExistingEntityError);
+				else
+					return new SuccessDataResult<RateOfEngine>(existingEntity);
+			}
+			catch (Exception ex)
+			{
+				return new ErrorDataResult<RateOfEngine>(ex.Message);
+			}
+		}
+	}
+}
