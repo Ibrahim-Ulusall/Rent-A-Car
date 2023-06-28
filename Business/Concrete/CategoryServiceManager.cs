4540ffe777c35cad49e9f0e2949e3e674671e044
ibrahim <ibrahimulusal185@gmail.com>
2023-06-28 19:48:18 +0300

    CategoryServiceManager ve CityServiceManager eklendi

---
+++ Business/Concrete/CategoryServiceManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Business.Contans;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+	public class CategoryServiceManager:ManagerBase<Category,ICategoryDal> , ICategoryService
+	{
+        public CategoryServiceManager(ICategoryDal categoryDal) : base(categoryDal) { }
+
+		public override IDataResult<Category> Get(Category entity)
+		{
+			try
+			{
+				Category existingEntity = _entityDal.Get(category => category.CategoryId == entity.CategoryId);
+				if (entity == null)
+					return new ErrorDataResult<Category>(Messages.NullEntityError);
+				else if (existingEntity == null)
+					return new ErrorDataResult<Category>(Messages.ExistingEntityError);
+				else
+					return new SuccessDataResult<Category>(existingEntity);
+			}
+			catch (Exception ex)
+			{
+				return new ErrorDataResult<Category>(ex.Message);
+			}
+		}
+	}
+}
---
+++ Business/Concrete/CityServiceManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Business.Contans;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+	public class CityServiceManager:ManagerBase<City,ICityDal> , ICityService
+	{
+		public CityServiceManager(ICityDal cityDal) : base(cityDal) { }
+
+		public override IDataResult<City> Get(City entity)
+		{
+			try
+			{
+				City existingEntity = _entityDal.Get(city => city.CityId == entity.CityId);
+				if (entity == null)
+					return new ErrorDataResult<City>(Messages.NullEntityError);
+				else if (existingEntity == null)
+					return new ErrorDataResult<City>(Messages.ExistingEntityError);
+				else
+					return new SuccessDataResult<City>(existingEntity);
+			}
+			catch (Exception ex)
+			{
+				return new ErrorDataResult<City>(ex.Message);
+			}
+		}
+	}
+}
